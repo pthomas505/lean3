2cdb8498238c9ae136d9dfdd070c145e87a516b3
Patrick Thomas <pthomas505@gmail.com>
2022-06-04 10:12:23 -0700

    Update.

--- src/metalogic/pred.lean
+++ src/metalogic/pred.lean
@@ -122,7 +122,38 @@ lemma finset.ne_imp_sdiff_union_comm
   (s \ {x}) ∪ {y} = (s ∪ {y}) \ {x} :=
 begin
   apply finset.ext, intros a,
-  simp only [finset.mem_union, finset.mem_sdiff, finset.mem_singleton], admit,
+  simp only [finset.mem_union, finset.mem_sdiff, finset.mem_singleton],
+  split,
+  {
+    intros h2, split,
+    {
+      cases h2,
+      {
+        cases h2, apply or.intro_left, exact h2_left
+      },
+      {
+        apply or.intro_right, exact h2
+      }
+    },
+    {
+      cases h2,
+      {
+        cases h2, exact h2_right
+      },
+      {
+        rewrite h2, rewrite ne_comm at h1, exact h1
+      }
+    }
+  },
+  {
+    intros h2, cases h2, cases h2_left,
+    {
+      apply or.intro_left, exact and.intro h2_left h2_right
+    },
+    {
+      apply or.intro_right, exact h2_left
+    }
+  }
 end
 
 
