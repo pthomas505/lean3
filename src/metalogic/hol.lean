f61b3ca996af8ae6d51bdcbc02d7039347abab04
Patrick Thomas <pthomas505@gmail.com>
2022-08-13 15:04:19 -0700

    Update.

--- src/metalogic/hol.lean
+++ src/metalogic/hol.lean
@@ -42,9 +42,9 @@ def hol_type.instance (type_var_symbol_to_type : type_var_symbols → hol_type) 
 
 lemma lem_1
 	(σ : hol_type)
-	(m₁ m₂ : type_var_symbols → hol_type)
-	(h1 : σ.instance m₁ = σ.instance m₂) :
-	∀ α ∈ σ.var_set, m₁ α = m₂ α :=
+	(τ₁ τ₂ : type_var_symbols → hol_type)
+	(h1 : σ.instance τ₁ = σ.instance τ₂) :
+	∀ α ∈ σ.var_set, τ₁ α = τ₂ α :=
 begin
 	intros α' h2,
 	induction σ,
@@ -54,11 +54,13 @@ begin
 		simp only [finset.mem_singleton] at h2, subst h2, exact h1,
 	},
   case hol_type.const : n ν args σ_ih
-  { unfold hol_type.instance at h1, unfold hol_type.var_set at h2,
-		simp at h1,
-		simp at h2, apply exists.elim h2, intros i h3,
-		apply σ_ih i, sorry, exact h3,
-},
+  {
+		unfold hol_type.instance at h1, unfold hol_type.var_set at h2,
+		simp only [eq_self_iff_true, heq_iff_eq, true_and] at h1,
+		simp only [finset.mem_bUnion, finset.mem_univ, exists_true_left] at h2,
+		apply exists.elim h2, intros i h3,
+		apply σ_ih i, apply congr_fun h1, exact h3
+	},
   case hol_type.func : σ₁ σ₂ σ₁_ih σ₂_ih
   {
 		unfold hol_type.instance at h1, unfold hol_type.var_set at h2,
@@ -66,5 +68,29 @@ begin
 		simp at h2, cases h2,
 		exact σ₁_ih h1_left h2,
 		exact σ₂_ih h1_right h2,
+	},
+end
+
+lemma lem_2
+	(σ : hol_type)
+	(τ : type_var_symbols → hol_type)
+	(M : model)
+	(V : valuation) :
+  M.type V (σ.instance τ) = M.type (λ i, M.type V (τ i)) σ :=
+begin
+	induction σ,
+	case hol_type.var : α
+  {
+		unfold hol_type.instance, unfold model.type
+	},
+  case hol_type.const : n ν args σ_ih
+  {
+		unfold hol_type.instance at *, unfold model.type at *,
+		congr, funext, apply σ_ih
+	},
+  case hol_type.func : σ₁ σ₂ σ₁_ih σ₂_ih
+  {
+		unfold hol_type.instance at *, unfold model.type at *,
+		rewrite σ₁_ih, rewrite σ₂_ih
 	},
 end
