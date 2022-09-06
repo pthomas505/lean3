3cb909ec34b1ca122a237274c7d8e3aa12734315
Patrick Thomas <pthomas505@gmail.com>
2022-09-05 22:39:26 -0700

    Update.

--- src/metalogic/sol.lean
+++ src/metalogic/sol.lean
@@ -1,4 +1,4 @@
-import data.finset
+import logic.function.basic
 
 set_option pp.parens true
 
@@ -45,19 +45,19 @@ structure assignment (D : Type) : Type :=
 (pred_var (n : ℕ) : pred_var_symbol → ((fin n → D) → Prop))
 
 
-structure interpretation (D : Type) : Type :=
+structure model (D : Type) : Type :=
 (nonempty : nonempty D)
 (func (n : ℕ) : func_symbol → ((fin n → D) → D))
 (pred (n : ℕ) : pred_symbol → ((fin n → D) → Prop))
 
 
-def eval_term (D : Type) (m : interpretation D) (v : assignment D) : term → D
+def eval_term (D : Type) (m : model D) (v : assignment D) : term → D
 | (var x) := v.var x
 | (func n f t) := m.func n f (fun (i : fin n), eval_term (t i))
 | (func_var n u t) := v.func_var n u (fun (i : fin n), eval_term (t i))
 
 
-def holds (D : Type) (m : interpretation D) : assignment D → formula → Prop
+def holds (D : Type) (m : model D) : assignment D → formula → Prop
 | _ bottom := false
 | _ top := true
 | v (pred n P t) := m.pred n P (fun (i : fin n), eval_term D m v (t i))
@@ -78,8 +78,8 @@ def holds (D : Type) (m : interpretation D) : assignment D → formula → Prop
 			var := function.update v.var x a,
 			func_var := v.func_var,
 			pred_var := v.pred_var } φ
-| v (forall_pred_var n X φ) := ∀ a : (pred_var_symbol → (fin n → D) → Prop),
+| v (forall_pred_var n X φ) := ∀ (a : (fin n → D) → Prop),
 		holds {
 			var := v.var,
 			func_var := v.func_var,
-			pred_var := fun (k : ℕ), function.update v.pred_var n a k } φ
+			pred_var := sorry } φ
