78aca36eccc00ccc7b3855c0696a68f2942bf63a
Patrick Thomas <pthomas505@gmail.com>
2022-12-07 01:04:05 -0800

    Update.

--- src/metalogic/fol_mm0.lean
+++ src/metalogic/fol_mm0.lean
@@ -992,6 +992,22 @@ def check_proof_step_list
   check_proof_step_list (local_proof_list ++ [local_proof]) proof_step_list
 
 
+inductive conv_step
+| refl_conv : conv_step
+| symm_conv : conv_step → conv_step
+| trans_conv : formula → conv_step → conv_step → conv_step
+| conv_not : conv_step → conv_step
+
+open conv_step
+
+def check_conv : conv_step → formula → formula → bool
+| refl_conv p q := p = q
+| (symm_conv step) p q := check_conv step q p
+| (trans_conv r step1 step2) p q := check_conv step1 p q ∧ check_conv step2 q r
+| (conv_not step) (not_ p) (not_ q) := check_conv step p q
+| _ _ _ := false
+
+
 -- Semantics
 
 
