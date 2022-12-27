eebe41343d123f6c9631eb66256d9deafc9a816a
Patrick Thomas <pthomas505@gmail.com>
2022-12-27 12:43:35 -0500

    Update.

--- src/metalogic/fol_mm0.lean
+++ src/metalogic/fol_mm0.lean
@@ -3393,8 +3393,14 @@ begin
     unfold mm0.formula.to_fol_formula,
     unfold fol.formula.subst,
   },
-  case mm0.formula.not_ : φ_ᾰ φ_ih
-  { admit },
+  case mm0.formula.not_ : φ φ_ih
+  {
+    unfold mm0.formula.subst,
+    unfold mm0.formula.to_fol_formula,
+    unfold fol.formula.subst,
+    congr,
+    exact φ_ih,
+  },
   case mm0.formula.imp_ : φ_ᾰ φ_ᾰ_1 φ_ih_ᾰ φ_ih_ᾰ_1
   { admit },
   case mm0.formula.eq_ : φ_ᾰ φ_ᾰ_1
