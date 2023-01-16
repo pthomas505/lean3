e74638da0a79793e471d4cd2c77680b75059648d
Patrick Thomas <pthomas505@gmail.com>
2023-01-15 23:05:34 -0800

    Update.

--- src/metalogic/fol_mm0.lean
+++ src/metalogic/fol_mm0.lean
@@ -5153,7 +5153,7 @@ begin
           rewrite to_fol_formula_env_ext _ _ _ _ _ c2,
           specialize E_ih (mm0.formula.def_ name args),
           unfold mm0.formula.subst at E_ih,
-          exact E_ih h1,
+          exact E_ih,
         }
       },
     },
