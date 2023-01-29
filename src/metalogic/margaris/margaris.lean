6c9bd7ad0267e28e18708b9ef88ee7fe412a3ecc
Patrick Thomas <pthomas505@gmail.com>
2023-01-28 23:37:34 -0800

    Update.

--- src/metalogic/margaris/margaris.lean
+++ src/metalogic/margaris/margaris.lean
@@ -11,39 +11,39 @@ inductive pred_symbol_ : Type
 
 
 @[derive decidable_eq]
-inductive formula_ : Type
-| pred_ : pred_symbol_ → list variable_ → formula_
-| not_ : formula_ → formula_
-| imp_ : formula_ → formula_ → formula_
-| forall_ : variable_ → formula_ → formula_
+inductive formula : Type
+| pred_ : pred_symbol_ → list variable_ → formula
+| not_ : formula → formula
+| imp_ : formula → formula → formula
+| forall_ : variable_ → formula → formula
 
-open formula_
+open formula
 
 
 -- P ∨ Q := ~ P → Q
-def formula_.or_ (P Q : formula_) : formula_ := (not_ P).imp_ Q
+def formula.or_ (P Q : formula) : formula := (not_ P).imp_ Q
 
 -- P ∧ Q := ~ ( P → ~ Q )
-def formula_.and_ (P Q : formula_) : formula_ := not_ (P.imp_ (not_ Q))
+def formula.and_ (P Q : formula) : formula := not_ (P.imp_ (not_ Q))
 
 -- P ↔ Q := ( P → Q ) ∧ ( Q → P )
-def formula_.iff_ (P Q : formula_) : formula_ := (P.imp_ Q).and_ (Q.imp_ P)
+def formula.iff_ (P Q : formula) : formula := (P.imp_ Q).and_ (Q.imp_ P)
 
 -- ∃ x P := ~ ∀ x ~ P
-def formula_.exists_ (x : variable_) (P : formula_) : formula_ := not_ (forall_ x (not_ P))
+def formula.exists_ (x : variable_) (P : formula) : formula := not_ (forall_ x (not_ P))
 
 
 /-
 An occurrence of a variable $v$ in a formula $P$ is bound if and only if it occurs in a subformula of $P$ of the form $\forall v Q$. An occurrence of $v$ in $P$ is free if and only if it is not a bound occurrence. The variable $v$ is free or bound in $P$ according as it has a free or bound occurrence in $P$.
 -/
 
-def formula_.free_var_set : formula_ → finset variable_
+def formula.free_var_set : formula → finset variable_
 | (pred_ name args) := args.to_finset
 | (not_ P) := P.free_var_set
 | (imp_ P Q) := P.free_var_set ∪ Q.free_var_set
 | (forall_ x P) := P.free_var_set \ {x}
 
-def is_free_in (v : variable_) : formula_ → Prop
+def is_free_in (v : variable_) : formula → Prop
 | (pred_ name args) := v ∈ args.to_finset
 | (not_ P) := is_free_in P
 | (imp_ P Q) := is_free_in P ∨ is_free_in Q
@@ -52,33 +52,33 @@ def is_free_in (v : variable_) : formula_ → Prop
 
 example
   (v : variable_)
-  (P : formula_) :
+  (P : formula) :
   is_free_in v P ↔ v ∈ P.free_var_set :=
 begin
   induction P,
-  case formula_.pred_ : name args
+  case formula.pred_ : name args
   {
     refl,
   },
-  case formula_.not_ : P P_ih
+  case formula.not_ : P P_ih
   {
     unfold is_free_in,
-    unfold formula_.free_var_set,
+    unfold formula.free_var_set,
     exact P_ih,
   },
-  case formula_.imp_ : P Q P_ih Q_ih
+  case formula.imp_ : P Q P_ih Q_ih
   {
     unfold is_free_in,
-    unfold formula_.free_var_set,
+    unfold formula.free_var_set,
     simp only [finset.mem_union],
     exact iff.or P_ih Q_ih,
   },
-  case formula_.forall_ : x P P_ih
+  case formula.forall_ : x P P_ih
   {
     cases P_ih,
 
     unfold is_free_in,
-    unfold formula_.free_var_set,
+    unfold formula.free_var_set,
     simp only [finset.mem_sdiff, finset.mem_singleton],
     split,
     {
@@ -119,7 +119,7 @@ def replace
   if x = v then t else x
 
 -- P (t/v)
-def replace_free (v t : variable_) : formula_ → formula_
+def replace_free (v t : variable_) : formula → formula
 | (pred_ name args) := pred_ name (args.map (replace v t))
 | (not_ P) := not_ (replace_free P)
 | (imp_ P Q) := imp_ (replace_free P) (replace_free Q)
@@ -134,54 +134,54 @@ If $v$ and $u$ are variables and $P$ is a formula, then $P$ admits $u$ for $v$ i
 -/
 
 -- P admits u for v
-def admits (v u : variable_) : formula_ → Prop
+def admits (v u : variable_) : formula → Prop
 | (pred_ name args) := true
 | (not_ P) := admits P
 | (imp_ P Q) := admits P ∧ admits Q
 | (forall_ x P) := x = v ∨ (¬ x = u ∧ admits P)
 
 
-inductive is_axiom : formula_ → Prop
+inductive is_axiom : formula → Prop
 
-| prop_1 (P Q : formula_) :
+| prop_1 (P Q : formula) :
   is_axiom (P.imp_ (Q.imp_ P))
 
-| prop_2 (P Q S : formula_) :
+| prop_2 (P Q S : formula) :
   is_axiom ((S.imp_ (P.imp_ Q)).imp_ ((S.imp_ P).imp_ (S.imp_ Q)))
 
-| prop_3 (P Q : formula_) :
+| prop_3 (P Q : formula) :
   is_axiom (((not_ Q).imp_ (not_ P)).imp_ (P.imp_ Q))
 
-| pred_1 (P Q : formula_) (v : variable_) :
+| pred_1 (P Q : formula) (v : variable_) :
   is_axiom ((forall_ v (P.imp_ Q)).imp_ ((forall_ v P).imp_ (forall_ v Q)))
 
-| pred_2 (v : variable_) (P : formula_) (t : variable_) :
+| pred_2 (v : variable_) (P : formula) (t : variable_) :
   -- $P$ admits $t$ for $v$
   admits v t P →
   -- $P(t/v)$
   is_axiom ((forall_ v P).imp_ (replace_free v t P))
 
-| pred_3 (P : formula_) (v : variable_) :
+| pred_3 (P : formula) (v : variable_) :
   -- $v$ is not free in $P$
   ¬ is_free_in v P →
   is_axiom (P.imp_ (forall_ v P))
 
-| gen (P : formula_) (v : variable_) :
+| gen (P : formula) (v : variable_) :
   is_axiom P →
   v ∈ P.free_var_set →
   is_axiom (forall_ v P)
 
 
-inductive is_deduct_from (Δ : finset formula_) : formula_ → Prop
-| axiom_ (P : formula_) :
+inductive is_deduct_from (Δ : finset formula) : formula → Prop
+| axiom_ (P : formula) :
   is_axiom P →
   is_deduct_from P
 
-| assumption_ (P : formula_) :
+| assumption_ (P : formula) :
   P ∈ Δ →
   is_deduct_from P
 
-| mp_ {P Q : formula_} :
+| mp_ {P Q : formula} :
   -- major premise
   is_deduct_from (P.imp_ Q) →
   -- minor premise
@@ -189,11 +189,11 @@ inductive is_deduct_from (Δ : finset formula_) : formula_ → Prop
   is_deduct_from Q
 
 
-def is_proof (P : formula_) : Prop := is_deduct_from ∅ P
+def is_proof (P : formula) : Prop := is_deduct_from ∅ P
 
 
 theorem thm_5
-  (P : formula_) :
+  (P : formula) :
   is_proof (P.imp_ P) :=
 begin
   have s1 : is_deduct_from ∅ ((P.imp_ ((P.imp_ P).imp_ P)).imp_ ((P.imp_ (P.imp_ P)).imp_ (P.imp_ P))),
