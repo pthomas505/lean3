62dfda5b8a521d0f13500156113e57fd6b756a00
Patrick Thomas <pthomas505@gmail.com>
2023-02-01 19:28:11 -0800

    Update.

--- src/metalogic/margaris/aux.lean
+++ src/metalogic/margaris/aux.lean
@@ -273,11 +273,19 @@ example
 begin
   induction P generalizing S,
   case formula.pred_ : name args S h1
-  { admit },
+  {
+
+  },
   case formula.not_ : P P_ih S h1
-  { admit },
+  {
+
+  },
   case formula.imp_ : P Q P_ih Q_ih S h1
-  { admit },
+  {
+
+  },
   case formula.forall_ : x P P_ih S h1
-  { admit },
+  {
+
+  },
 end
