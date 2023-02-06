f66b4731bf31da1f062cacff3a41d7d725feec29
Patrick Thomas <pthomas505@gmail.com>
2023-02-06 15:53:51 -0800

    Update.

--- src/metalogic/margaris/aux.lean
+++ src/metalogic/margaris/aux.lean
@@ -642,8 +642,15 @@ begin
       sorry,
     },
   },
-  case formula.not_ : P_ᾰ P_ih binders h1
-  { admit },
+  case formula.not_ : P P_ih binders h1
+  {
+    unfold admits_aux at h1,
+
+    unfold replace_free,
+    unfold to_is_bound_aux,
+    congr' 1,
+    exact P_ih binders h1,
+  },
   case formula.imp_ : P_ᾰ P_ᾰ_1 P_ih_ᾰ P_ih_ᾰ_1 binders h1
   { admit },
   case formula.forall_ : x P P_ih binders h1
