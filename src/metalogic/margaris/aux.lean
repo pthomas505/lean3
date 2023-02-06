ed386b72595d3822ba3bdae38168c8738c92f33c
Patrick Thomas <pthomas505@gmail.com>
2023-02-06 11:36:01 -0800

    Update.

--- src/metalogic/margaris/aux.lean
+++ src/metalogic/margaris/aux.lean
@@ -36,6 +36,7 @@ pg. 48
 An occurrence of a variable $v$ in a formula $P$ is bound if and only if it occurs in a subformula of $P$ of the form $\forall v Q$. An occurrence of $v$ in $P$ is free if and only if it is not a bound occurrence. The variable $v$ is free or bound in $P$ according as it has a free or bound occurrence in $P$.
 -/
 
+
 def formula.bound_var_set : formula → finset variable_
 | (pred_ name args) := ∅
 | (not_ P) := P.bound_var_set
@@ -48,6 +49,7 @@ def is_bound_in (v : variable_) : formula → Prop
 | (imp_ P Q) := is_bound_in P ∨ is_bound_in Q
 | (forall_ x P) := v = x ∨ is_bound_in P
 
+
 example
   (v : variable_)
   (P : formula) :
@@ -55,13 +57,55 @@ example
 begin
   induction P,
   case formula.pred_ : name args
-  { admit },
+  {
+    refl,
+  },
   case formula.not_ : P P_ih
-  { admit },
+  {
+    unfold is_bound_in,
+    unfold formula.bound_var_set,
+    exact P_ih,
+  },
   case formula.imp_ : P Q P_ih Q_ih
-  { admit },
+  {
+    unfold is_bound_in,
+    unfold formula.bound_var_set,
+    simp only [finset.mem_union],
+    exact iff.or P_ih Q_ih,
+  },
   case formula.forall_ : x P P_ih
-  { admit },
+  {
+    cases P_ih,
+
+    unfold is_bound_in,
+    unfold formula.bound_var_set,
+    simp only [finset.mem_union, finset.mem_singleton],
+    split,
+    {
+      intros a1,
+      cases a1,
+      {
+        apply or.intro_right,
+        exact a1,
+      },
+      {
+        apply or.intro_left,
+        exact P_ih_mp a1,
+      }
+    },
+    {
+      intros a1,
+      cases a1,
+      {
+        apply or.intro_right,
+        exact P_ih_mpr a1,
+      },
+      {
+        apply or.intro_left,
+        exact a1,
+      }
+    }
+  },
 end
 
 
