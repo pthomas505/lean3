ea08e9cdc400fb1b3f8514ab7781e882cabf9d18
Patrick Thomas <pthomas505@gmail.com>
2023-02-07 15:08:13 -0800

    Update.

--- src/metalogic/margaris/aux.lean
+++ src/metalogic/margaris/aux.lean
@@ -1200,9 +1200,9 @@ begin
   {
     induction args,
     case list.nil
-  { admit },
-  case list.cons : args_hd args_tl args_ih
-  { admit },
+    { admit },
+    case list.cons : args_hd args_tl args_ih
+    { admit },
   },
   case formula.not_ : P P_ih binders h1 h2
   {
