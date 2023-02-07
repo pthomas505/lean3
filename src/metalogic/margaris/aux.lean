4e0d4148994c58a400de02b5d0da970a6cb688d3
Patrick Thomas <pthomas505@gmail.com>
2023-02-07 14:57:23 -0800

    Update.

--- src/metalogic/margaris/aux.lean
+++ src/metalogic/margaris/aux.lean
@@ -1224,6 +1224,13 @@ example
   fast_admits_aux v u binders P :=
 begin
   induction P generalizing binders,
-
+  case formula.pred_ : P_ᾰ P_ᾰ_1 binders h1 h2
+  { admit },
+  case formula.not_ : P_ᾰ P_ih binders h1 h2
+  { admit },
+  case formula.imp_ : P_ᾰ P_ᾰ_1 P_ih_ᾰ P_ih_ᾰ_1 binders h1 h2
+  { admit },
+  case formula.forall_ : P_ᾰ P_ᾰ_1 P_ih binders h1 h2
+  { admit },
 end
 
