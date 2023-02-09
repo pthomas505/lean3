b703bd5e35aa54c55fbfe96b25b3fc01ae3916cd
Patrick Thomas <pthomas505@gmail.com>
2023-02-09 14:44:00 -0800

    Update.

--- src/metalogic/fol/aux.lean
+++ src/metalogic/fol/aux.lean
@@ -1412,8 +1412,10 @@ begin
   { admit },
   case formula.imp_ : P_ᾰ P_ᾰ_1 P_ih_ᾰ P_ih_ᾰ_1 binders P' h1 h2
   { admit },
-  case formula.forall_ : P_ᾰ P_ᾰ_1 P_ih binders P' h1 h2
-  { admit },
+  case formula.forall_ : x P P_ih binders P' h1 h2
+  {
+
+  },
 end
 
 
