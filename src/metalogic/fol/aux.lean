3084715d4da4a89cf7b41b74e5518d382a4da228
Patrick Thomas <pthomas505@gmail.com>
2023-02-09 18:31:09 -0800

    Update.

--- src/metalogic/fol/aux.lean
+++ src/metalogic/fol/aux.lean
@@ -1540,6 +1540,8 @@ lemma is_prop_sub_imp_fast_admits_aux
 begin
   apply exists.elim h1,
   intros P' h1_1,
+  clear h1,
+
   induction h1_1 generalizing binders,
   case is_prop_sub'.pred_ : h1_1_name h1_1_args h1_1_v h1_1_t binders
   { admit },
@@ -1547,8 +1549,12 @@ begin
   { admit },
   case is_prop_sub'.imp_ : h1_1_P h1_1_Q h1_1_v h1_1_t h1_1_P' h1_1_Q' h1_1_ᾰ h1_1_ᾰ_1 h1_1_ih_ᾰ h1_1_ih_ᾰ_1 binders
   { admit },
-  case is_prop_sub'.forall_same : h1_1_x h1_1_P h1_1_v h1_1_t h1_1_P' h1_1_ᾰ binders
-  { admit },
+  case is_prop_sub'.forall_same : h1_1_x h1_1_P h1_1_v h1_1_t h1_1_P' h1_1_1 binders
+  {
+    unfold fast_admits_aux,
+    apply or.intro_left,
+    exact h1_1_1,
+  },
   case is_prop_sub'.forall_diff_nel : h1_1_x h1_1_P h1_1_v h1_1_t h1_1_P' h1_1_ᾰ h1_1_ᾰ_1 h1_1_ᾰ_2 h1_1_ih binders
   { admit },
   case is_prop_sub'.forall_diff : h1_1_x h1_1_P h1_1_v h1_1_t h1_1_P' h1_1_ᾰ h1_1_ᾰ_1 h1_1_ᾰ_2 h1_1_ih binders
