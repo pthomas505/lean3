687979a183b378fa3c99fbcb238f7998d89db412
Patrick Thomas <pthomas505@gmail.com>
2023-02-09 20:10:14 -0800

    Update.

--- src/metalogic/fol/aux.lean
+++ src/metalogic/fol/aux.lean
@@ -1555,8 +1555,10 @@ begin
     unfold fast_admits_aux,
     exact h1_1_ih binders h2,
   },
-  case is_prop_sub'.imp_ : h1_1_P h1_1_Q h1_1_v h1_1_t h1_1_P' h1_1_Q' h1_1_ᾰ h1_1_ᾰ_1 h1_1_ih_ᾰ h1_1_ih_ᾰ_1 binders h2
-  { admit },
+  case is_prop_sub'.imp_ : h1_1_P h1_1_Q h1_1_v h1_1_t h1_1_P' h1_1_Q' h1_1_1 h1_1_2 h1_1_ih_1 h1_1_ih_2 binders h2
+  {
+
+  },
   case is_prop_sub'.forall_same : h1_1_x h1_1_P h1_1_v h1_1_t h1_1_P' h1_1_1 binders h2
   {
     unfold fast_admits_aux,
