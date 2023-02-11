11e735a3deca922e3fa0b822b333f7dc8d36ee89
Patrick Thomas <pthomas505@gmail.com>
2023-02-10 21:18:36 -0800

    Update.

--- src/metalogic/fol/margaris/margaris.lean
+++ src/metalogic/fol/margaris/margaris.lean
@@ -174,19 +174,19 @@ begin
   {
     induction h1_1,
     case is_axiom.prop_1 : h1_1_P h1_1_Q
-  { admit },
-  case is_axiom.prop_2 : h1_1_P h1_1_Q h1_1_S
-  { admit },
-  case is_axiom.prop_3 : h1_1_P h1_1_Q
-  { admit },
-  case is_axiom.pred_1 : h1_1_P h1_1_Q h1_1_v
-  { admit },
-  case is_axiom.pred_2 : h1_1_v h1_1_P h1_1_t h1_1_ᾰ
-  { admit },
-  case is_axiom.pred_3 : h1_1_P h1_1_v h1_1_ᾰ
-  { admit },
-  case is_axiom.gen : h1_1_P h1_1_v h1_1_ᾰ h1_1_ᾰ_1 h1_1_ih
-  { admit },
+    { admit },
+    case is_axiom.prop_2 : h1_1_P h1_1_Q h1_1_S
+    { admit },
+    case is_axiom.prop_3 : h1_1_P h1_1_Q
+    { admit },
+    case is_axiom.pred_1 : h1_1_P h1_1_Q h1_1_v
+    { admit },
+    case is_axiom.pred_2 : h1_1_v h1_1_P h1_1_t h1_1_ᾰ
+    { admit },
+    case is_axiom.pred_3 : h1_1_P h1_1_v h1_1_ᾰ
+    { admit },
+    case is_axiom.gen : h1_1_P h1_1_v h1_1_ᾰ h1_1_ᾰ_1 h1_1_ih
+    { admit },
   },
   case is_deduct.assumption_ : h1_P h1_ᾰ
   { admit },
