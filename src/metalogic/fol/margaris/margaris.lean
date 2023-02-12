017227766011ad7f2f0a094e41a54a0a9079be05
Patrick Thomas <pthomas505@gmail.com>
2023-02-11 18:48:35 -0800

    Update.

--- src/metalogic/fol/margaris/margaris.lean
+++ src/metalogic/fol/margaris/margaris.lean
@@ -12,38 +12,39 @@ open formula
 
 inductive is_proof (Δ : set formula) : formula → Prop
 
--- |- (p -> (q -> p))
+-- ⊢ (P → (Q → P))
 | prop_1_
   (P Q : formula) :
   is_proof (P.imp_ (Q.imp_ P))
 
--- |- ((p -> (q -> r)) -> ((p -> q) -> (p -> r)))
+-- ⊢ ((P → (Q → R)) → ((P → Q) → (P → R)))
 | prop_2_
   (P Q R : formula) :
   is_proof ((P.imp_ (Q.imp_ R)).imp_ ((P.imp_ Q).imp_ (P.imp_ R)))
 
--- |- ((~p -> ~q) -> (q -> p))
+-- ⊢ ((¬ P → ¬ Q) → (Q → P))
 | prop_3_
   (P Q : formula) :
   is_proof (((not_ P).imp_ (not_ Q)).imp_ (Q.imp_ P))
 
+-- ⊢ ((∀ v (P → Q)) → ((∀ v P) → (∀ v Q)))
 | pred_1_
   (P Q : formula) (v : variable_) :
   is_proof ((forall_ v (P.imp_ Q)).imp_ ((forall_ v P).imp_ (forall_ v Q)))
 
+-- ⊢ ((∀ v P) → P(t/v)) provided P admits t for v
 | pred_2_
   (v : variable_) (P : formula) (t : variable_) :
-  -- $P$ admits $t$ for $v$
   admits v t P →
-  -- $P(t/v)$
   is_proof ((forall_ v P).imp_ (replace_free v t P))
 
+-- ⊢ (P → (∀ v P)) provided v is not free in P
 | pred_3_
   (P : formula) (v : variable_) :
-  -- $v$ is not free in $P$
   ¬ is_free_in v P →
   is_proof (P.imp_ (forall_ v P))
 
+-- ⊢ P ⇒ ⊢ (∀ v P) provided v is free in P
 | gen_
   (P : formula) (v : variable_) :
   ∀ (H : formula), H ∈ Δ → v ∉ H.free_var_set →
@@ -51,6 +52,7 @@ inductive is_proof (Δ : set formula) : formula → Prop
   is_proof P →
   is_proof (forall_ v P)
 
+-- ⊢ (P → Q) ⇒ ⊢ P ⇒ ⊢ Q 
 | mp_
   {P Q : formula} :
   -- major premise
@@ -59,6 +61,7 @@ inductive is_proof (Δ : set formula) : formula → Prop
   is_proof P →
   is_proof Q
 
+-- Δ ∪ {P} ⊢ P
 | assume_
   (P : formula) :
   P ∈ Δ →
