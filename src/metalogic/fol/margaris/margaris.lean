6d092ba43789ae78dcabed7b90d5a9ea744768a0
Patrick Thomas <pthomas505@gmail.com>
2023-02-12 00:00:00 -0800

    Update.

--- src/metalogic/fol/margaris/margaris.lean
+++ src/metalogic/fol/margaris/margaris.lean
@@ -191,15 +191,15 @@ begin
     {
       -- Case 2
 
-      have s1 : is_deduct Δ h1_P,
-      apply is_deduct.assume_,
-      exact h1_1,
-
-      have s2 : is_deduct Δ (h1_P.imp_ (P.imp_ h1_P)),
-      apply is_deduct.axiom_,
-      apply is_axiom.prop_1_,
-
-      exact is_deduct.mp_ h1_P (imp_ P h1_P) s2 s1,
+      apply is_deduct.mp_,
+      {
+        apply is_deduct.axiom_,
+        exact is_axiom.prop_1_ h1_P P,
+      },
+      {
+        apply is_deduct.assume_,
+        exact h1_1,
+      },
     },
     {
       -- Case 3
