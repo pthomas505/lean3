dcb2c90779b2bf2033d34bb2d2ffef295a604192
Patrick Thomas <pthomas505@gmail.com>
2023-02-14 17:59:28 -0800

    Update.

--- src/metalogic/fol/current/fol.lean
+++ src/metalogic/fol/current/fol.lean
@@ -474,3 +474,115 @@ begin
 
   exact is_deduct.mp_ P Q s2 s1,
 end
+
+
+
+inductive is_proof_alt : formula → Prop
+
+-- ⊢ P → (Q → P)
+| prop_1_
+  (P Q : formula) :
+  is_proof_alt (P.imp_ (Q.imp_ P))
+
+-- ⊢ (P → (Q → R)) → ((P → Q) → (P → R))
+| prop_2_
+  (P Q R : formula) :
+  is_proof_alt ((P.imp_ (Q.imp_ R)).imp_ ((P.imp_ Q).imp_ (P.imp_ R)))
+
+-- ⊢ (¬ P → ¬ Q) → (Q → P)
+| prop_3_
+  (P Q : formula) :
+  is_proof_alt (((not_ P).imp_ (not_ Q)).imp_ (Q.imp_ P))
+
+-- ⊢ (∀ v (P → Q)) → ((∀ v P) → (∀ v Q))
+| pred_1_
+  (P Q : formula) (v : variable_) :
+  is_proof_alt ((forall_ v (P.imp_ Q)).imp_ ((forall_ v P).imp_ (forall_ v Q)))
+
+-- ⊢ (∀ v P) → P(t/v)  provided P admits t for v
+| pred_2_
+  (v : variable_) (P : formula) (t : variable_) :
+  admits v t P →
+  is_proof_alt ((forall_ v P).imp_ (replace_free v t P))
+
+-- ⊢ P → (∀ v P)  provided v is not free in P
+| pred_3_
+  (P : formula) (v : variable_) :
+  ¬ is_free_in v P →
+  is_proof_alt (P.imp_ (forall_ v P))
+
+-- ⊢ P ⇒ ⊢ ∀ v P
+| gen_
+  (P : formula) (v : variable_) :
+  is_proof_alt P →
+  is_proof_alt (forall_ v P)
+
+| mp_
+  (P Q : formula) :
+  is_proof_alt (P.imp_ Q) →
+  is_proof_alt P →
+  is_proof_alt Q
+
+
+lemma generalization
+  (P : formula)
+  (v : variable_)
+  (Δ : set formula)
+  (h1 : is_deduct Δ P)
+  (h2 : ∀ (H : formula), H ∈ Δ → v ∉ H.free_var_set) :
+  is_deduct Δ (forall_ v P) := sorry
+
+
+example
+  (P : formula)
+  (h1 : is_proof_alt P) :
+  is_deduct ∅ P :=
+begin
+  induction h1,
+  case is_proof_alt.prop_1_ : h1_P h1_Q
+  {
+    apply is_deduct.axiom_,
+    apply is_axiom.prop_1_,
+  },
+  case is_proof_alt.prop_2_ : h1_P h1_Q h1_R
+  {
+    apply is_deduct.axiom_,
+    apply is_axiom.prop_2_,
+  },
+  case is_proof_alt.prop_3_ : h1_P h1_Q
+  {
+    apply is_deduct.axiom_,
+    apply is_axiom.prop_3_,
+  },
+  case is_proof_alt.pred_1_ : h1_P h1_Q h1_v
+  {
+    apply is_deduct.axiom_,
+    apply is_axiom.pred_1_,
+  },
+  case is_proof_alt.pred_2_ : h1_v h1_P h1_t h1_1
+  {
+    apply is_deduct.axiom_,
+    apply is_axiom.pred_2_,
+    exact h1_1,
+  },
+  case is_proof_alt.pred_3_ : h1_P h1_v h1_1
+  {
+    apply is_deduct.axiom_,
+    apply is_axiom.pred_3_,
+    exact h1_1,
+  },
+  case is_proof_alt.gen_ : h1_P h1_v h1_1 h1_ih
+  {
+    apply generalization,
+    {
+      exact h1_ih,
+    },
+    {
+      simp only [set.mem_empty_eq, is_empty.forall_iff, forall_const],
+    }
+  },
+  case is_proof_alt.mp_ : h1_P h1_Q h1_1 h1_2 h1_ih_1 h1_ih_2
+  {
+    exact is_deduct.mp_ h1_P h1_Q h1_ih_1 h1_ih_2,
+  },
+end
