8a6ad217cc36f504123ecdae772cae8cc178a1fc
Patrick Thomas <pthomas505@gmail.com>
2023-02-15 16:11:01 -0800

    Update.

--- src/metalogic/fol/current/binders.lean
+++ src/metalogic/fol/current/binders.lean
@@ -1,39 +1,12 @@
+import metalogic.fol.current.formula
+
 import data.finset
+
 
 set_option pp.parens true
 
 
-@[derive [inhabited, decidable_eq]]
-inductive variable_ : Type
-| variable_ : string → variable_
-
-
-@[derive [inhabited, decidable_eq]]
-inductive pred_name_ : Type
-| pred_name_ : string → pred_name_
-
-
-@[derive [inhabited, decidable_eq]]
-inductive formula : Type
-| pred_ : pred_name_ → list variable_ → formula
-| not_ : formula → formula
-| imp_ : formula → formula → formula
-| forall_ : variable_ → formula → formula
-
 open formula
-
-
--- P ∨ Q := ~ P → Q
-def formula.or_ (P Q : formula) : formula := (not_ P).imp_ Q
-
--- P ∧ Q := ~ ( P → ~ Q )
-def formula.and_ (P Q : formula) : formula := not_ (P.imp_ (not_ Q))
-
--- P ↔ Q := ( P → Q ) ∧ ( Q → P )
-def formula.iff_ (P Q : formula) : formula := (P.imp_ Q).and_ (Q.imp_ P)
-
--- ∃ x P := ~ ∀ x ~ P
-def formula.exists_ (x : variable_) (P : formula) : formula := not_ (forall_ x (not_ P))
 
 
 /-
