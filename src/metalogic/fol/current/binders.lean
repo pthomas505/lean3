5c862297da64d57c15b7831af11084d474d9f9b1
Patrick Thomas <pthomas505@gmail.com>
2023-02-19 15:16:55 -0800

    Update.

--- src/metalogic/fol/current/binders.lean
+++ src/metalogic/fol/current/binders.lean
@@ -387,10 +387,29 @@ begin
       }
     }
   },
-  case formula.not_ : P_ᾰ P_ih binders
-  { admit },
-  case formula.imp_ : P_ᾰ P_ᾰ_1 P_ih_ᾰ P_ih_ᾰ_1 binders
-  { admit },
+  case formula.not_ : P P_ih binders
+  {
+    unfold occurs_in at h1,
+
+    unfold replace_free_aux,
+    congr,
+    exact P_ih h1 binders h2,
+  },
+  case formula.imp_ : P Q P_ih Q_ih binders
+  {
+    unfold occurs_in at h1,
+    push_neg at h1,
+    cases h1,
+
+    unfold replace_free_aux,
+    congr,
+    {
+      exact P_ih h1_left binders h2,
+    },
+    {
+      exact Q_ih h1_right binders h2,
+    }
+  },
   case formula.forall_ : x P P_ih binders
   {
     unfold occurs_in at h1,
