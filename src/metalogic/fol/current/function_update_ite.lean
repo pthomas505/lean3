0b84629cd09dbd08fc4a32e4434ba4f7aa8c8ae8
Patrick Thomas <pthomas505@gmail.com>
2023-03-13 19:17:18 -0700

    Update.

--- src/metalogic/fol/current/function_update_ite.lean
+++ src/metalogic/fol/current/function_update_ite.lean
@@ -12,7 +12,7 @@ def function.update_ite
   if a = a' then b else f a
 
 
-lemma comp_update_ite
+lemma function.update_ite_comp
   {α α' β : Sort*}
   [decidable_eq α]
   (f : α' → β)
@@ -26,4 +26,45 @@ begin
   simp only [function.comp_app],
   unfold function.update_ite,
   split_ifs; refl,
+end
+
+
+@[simp]
+lemma function.update_ite_idem
+  {α β : Type}
+  [decidable_eq α]
+  (f : α → β)
+  (a : α)
+  (x y : β)  :
+  function.update_ite (function.update_ite f a x) a y =
+    function.update_ite f a y :=
+begin
+  funext,
+  unfold function.update_ite,
+  split_ifs,
+  {
+    refl,
+  },
+  {
+    refl,
+  }
+end
+
+
+lemma function.update_ite_id
+  {α : Type}
+  [decidable_eq α]
+  (x : α) :
+  function.update_ite (id : α → α) x x = id :=
+begin
+  funext,
+  unfold function.update_ite,
+  split_ifs,
+  {
+    subst h,
+    simp only [id.def],
+  },
+  {
+    refl,
+  }
 end
