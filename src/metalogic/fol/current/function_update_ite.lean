41101d307259d99f91988bf7d09415fbb2a8d62c
Patrick Thomas <pthomas505@gmail.com>
2023-03-13 20:10:12 -0700

    Update.

--- src/metalogic/fol/current/function_update_ite.lean
+++ src/metalogic/fol/current/function_update_ite.lean
@@ -68,3 +68,74 @@ begin
     refl,
   }
 end
+
+
+lemma function.update_ite_not_mem_list
+  {α β : Type}
+  [decidable_eq α]
+  (l : list α)
+  (f : α → β)
+  (a : α)
+  (b : β)
+  (h1 : a ∉ l) :
+  l.map (function.update_ite f a b) = l.map f :=
+begin
+  induction l,
+  case list.nil
+  {
+    simp only [list.map_nil],
+  },
+  case list.cons : l_hd l_tl l_ih
+  {
+    simp only [list.mem_cons_iff] at h1,
+    push_neg at h1,
+    dsimp at h1,
+    cases h1,
+
+    simp only [list.map],
+    split,
+    {
+      unfold function.update_ite,
+      split_ifs; tauto,
+    },
+    {
+      exact l_ih h1_right,
+    }
+  },
+end
+
+
+lemma function.update_ite_not_mem_set
+  {α β : Type}
+  [decidable_eq α] [decidable_eq β]
+  (S : finset α)
+  (f : α → β)
+  (a : α)
+  (b : β)
+  (h1 : a ∉ S) :
+  finset.image (function.update_ite f a b) S = finset.image f S :=
+begin
+  induction S using finset.induction_on,
+  case h₁
+  {
+    simp only [finset.image_empty],
+  },
+  case h₂ : S_a S_S S_1 S_ih
+  {
+    simp only [finset.mem_insert] at h1,
+    push_neg at h1,
+    dsimp at h1,
+    cases h1,
+
+
+    simp only [finset.image_insert],
+    congr' 1,
+    {
+      unfold function.update_ite,
+      split_ifs; tauto,
+    },
+    {
+      exact S_ih h1_right,
+    }
+  },
+end
