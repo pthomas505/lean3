b21834e80d6fe3666b8592973f8144030d81e410
Patrick Thomas <pthomas505@gmail.com>
2023-03-29 22:36:48 -0700

    Update.

--- src/metalogic/fol/fol.lean
+++ src/metalogic/fol/fol.lean
@@ -10,6 +10,22 @@ open formula
 def proof_equiv (P Q : formula) : Prop := is_proof (P.iff_ Q)
 
 
+def is_repl_of_var_in_list_fun (u v : variable_) : list variable_ → list variable_ → Prop
+| [] [] := true
+| (hd_u :: tl_u) (hd_v :: tl_v) := (hd_u = hd_v ∨ (hd_u = u ∧ hd_v = v)) ∧ is_repl_of_var_in_list_fun tl_u tl_v
+| _ _ := false
+
+
+def is_repl_of_var_fun (u v : variable_) : formula → formula → Prop
+| true_ true_ := true
+| (pred_ name_u args_u) (pred_ name_v args_v) := name_u = name_v ∧ is_repl_of_var_in_list_fun u v args_u args_v
+| (eq_ x_u y_u) (eq_ x_v y_v) := (x_u = x_v ∨ (x_u = u ∧ x_v = v)) ∧ (y_u = y_v ∨ (y_u = u ∧ y_v = v))
+| (not_ P_u) (not_ P_v) := is_repl_of_var_fun P_u P_v
+| (imp_ P_u Q_u) (imp_ P_v Q_v) := is_repl_of_var_fun P_u P_v ∧ is_repl_of_var_fun Q_u Q_v
+| (forall_ x P_u) (forall_ x' P_v) := x = x' ∧ is_repl_of_var_fun P_u P_v
+| _ _ := false
+
+
 inductive is_repl_of_var (u v : variable_) : formula → formula → Prop
 | true_ :
   is_repl_of_var true_ true_
@@ -22,11 +38,11 @@ inductive is_repl_of_var (u v : variable_) : formula → formula → Prop
   is_repl_of_var (pred_ name (list.of_fn args_u)) (pred_ name (list.of_fn args_v))
 
 | eq_
-  (x y : variable_)
-  (x' y' : variable_) :
-  (x = x') ∨ (x = u ∧ x' = v) →
-  (y = y') ∨ (y = u ∧ y' = v) →
-  is_repl_of_var (eq_ x y) (eq_ x' y')
+  (x_u y_u : variable_)
+  (x_v y_v : variable_) :
+  (x_u = x_v) ∨ (x_u = u ∧ x_v = v) →
+  (y_u = y_v) ∨ (y_u = u ∧ y_v = v) →
+  is_repl_of_var (eq_ x_u y_u) (eq_ x_v y_v)
 
 | not_
   (P_u P_v : formula) :
@@ -50,10 +66,10 @@ inductive is_repl_of_var (u v : variable_) : formula → formula → Prop
 /--
 is_repl U V P_u P_v = True if and only if P_v is the result of replacing one or more specified occurrences (but not necessarily all occurrences) of U in P_u by occurrences of V.
 -/
-def is_repl_of_formula_alt (U V : formula) : formula → formula → Prop
-| (not_ P_u) (not_ P_v) := is_repl_of_formula_alt P_u P_v
-| (imp_ P_u Q_u) (imp_ P_v Q_v) := is_repl_of_formula_alt P_u P_v ∧ is_repl_of_formula_alt Q_u Q_v
-| (forall_ x P_u) (forall_ x' P_v) := x = x' ∧ is_repl_of_formula_alt P_u P_v
+def is_repl_of_formula_fun (U V : formula) : formula → formula → Prop
+| (not_ P_u) (not_ P_v) := is_repl_of_formula_fun P_u P_v
+| (imp_ P_u Q_u) (imp_ P_v Q_v) := is_repl_of_formula_fun P_u P_v ∧ is_repl_of_formula_fun Q_u Q_v
+| (forall_ x P_u) (forall_ x' P_v) := x = x' ∧ is_repl_of_formula_fun P_u P_v
 | P_u P_v := P_u = P_v ∨ (P_u = U ∧ P_v = V)
 
 
