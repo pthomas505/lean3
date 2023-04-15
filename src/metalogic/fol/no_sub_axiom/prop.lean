3a8de36910ad1f97aaf9642fa17766f0fbca408c
Patrick Thomas <pthomas505@gmail.com>
2023-04-15 11:20:26 -0700

    Update.

--- src/metalogic/fol/no_sub_axiom/prop.lean
+++ src/metalogic/fol/no_sub_axiom/prop.lean
@@ -7,4 +7,24 @@ set_option pp.parens true
 open formula
 
 
+theorem prop_refl
+  (P : formula) :
+  is_proof (P.imp_ P) :=
+begin
+  apply is_proof.mp_ (P.imp_ (P.imp_ P)),
+  {
+    apply is_proof.mp_ (P.imp_ ((P.imp_ P).imp_ P)),
+    {
+      exact is_proof.prop_2_ P (P.imp_ P) P,
+    },
+    {
+      exact is_proof.prop_1_ P (P.imp_ P),
+    }
+  },
+  {
+    exact is_proof.prop_1_ P P,
+  },
+end
+
+
 #lint
