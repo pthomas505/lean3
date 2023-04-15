04ada94b49f6cc3b07fb6014855b137173174f2e
Patrick Thomas <pthomas505@gmail.com>
2023-04-14 19:30:41 -0700

    Update.

--- src/metalogic/fol/no_sub_axiom/deduct.lean
+++ src/metalogic/fol/no_sub_axiom/deduct.lean
@@ -100,23 +100,23 @@ inductive is_axiom : formula → Prop
   is_axiom (exists_ x (eq_ x y))
 
 /-
-⊢ ∀ x_0 ... ∀ x_n ∀ y_0 ... y_n ((x_0 = y_0) ∧ ... ∧ (x_n = y_n) ∧ ⊤) →
+⊢ ((x_0 = y_0) ∧ ... ∧ (x_n = y_n) ∧ ⊤) →
     ((pred_ name [x_0 ... x_n] ↔ pred_ name [y_0 ... y_n]))
 -/
 | eq_2_pred_
   (name : pred_name_) (n : ℕ) (xs ys : fin n → variable_) :
-  is_axiom (Forall_ (list.of_fn xs) (Forall_ (list.of_fn ys)
+  is_axiom
     ((And_ (list.of_fn (fun (i : fin n), eq_ (xs i) (ys i)))).imp_
-      ((pred_ name (list.of_fn xs)).iff_ (pred_ name (list.of_fn ys))))))
+      ((pred_ name (list.of_fn xs)).iff_ (pred_ name (list.of_fn ys))))
 
 /-
-⊢ ∀ x_0 ∀ x_1 ∀ y_0 ∀ y_1 ((x_0 = y_0) ∧ (x_1 = y_1)) →
+⊢ ((x_0 = y_0) ∧ (x_1 = y_1)) →
     ((eq_ x_0 x_1) ↔ (eq_ y_0 y_1))
 -/
 | eq_2_eq_
   (x_0 x_1 y_0 y_1 : variable_) :
-  is_axiom (forall_ x_0 (forall_ x_1 (forall_ y_0 (forall_ y_1 ((and_ (eq_ x_0 y_0) (eq_ x_1 y_1)).imp_
-    ((eq_ x_0 x_1).iff_ (eq_ y_0 y_1)))))))
+  is_axiom ((and_ (eq_ x_0 y_0) (eq_ x_1 y_1)).imp_
+    ((eq_ x_0 x_1).iff_ (eq_ y_0 y_1)))
 
 -- ⊢ P ⇒ ⊢ ∀ v P
 | gen_
@@ -195,23 +195,23 @@ inductive is_proof_alt : formula → Prop
   is_proof_alt (exists_ x (eq_ x y))
 
 /-
-⊢ ∀ x_0 ... ∀ x_n ∀ y_0 ... y_n ((x_0 = y_0) ∧ ... ∧ (x_n = y_n) ∧ ⊤) →
+⊢ ((x_0 = y_0) ∧ ... ∧ (x_n = y_n) ∧ ⊤) →
     ((pred_ name [x_0 ... x_n] ↔ pred_ name [y_0 ... y_n]))
 -/
 | eq_2_pred_
   (name : pred_name_) (n : ℕ) (xs ys : fin n → variable_) :
-  is_proof_alt (Forall_ (list.of_fn xs) (Forall_ (list.of_fn ys)
+  is_proof_alt
     ((And_ (list.of_fn (fun (i : fin n), eq_ (xs i) (ys i)))).imp_
-      ((pred_ name (list.of_fn xs)).iff_ (pred_ name (list.of_fn ys))))))
+      ((pred_ name (list.of_fn xs)).iff_ (pred_ name (list.of_fn ys))))
 
 /-
-⊢ ∀ x_0 ∀ x_1 ∀ y_0 ∀ y_1 ((x_0 = y_0) ∧ (x_1 = y_1)) →
+⊢ ((x_0 = y_0) ∧ (x_1 = y_1)) →
     ((eq_ x_0 x_1) ↔ (eq_ y_0 y_1))
 -/
 | eq_2_eq_
   (x_0 x_1 y_0 y_1 : variable_) :
-  is_proof_alt (forall_ x_0 (forall_ x_1 (forall_ y_0 (forall_ y_1 ((and_ (eq_ x_0 y_0) (eq_ x_1 y_1)).imp_
-    ((eq_ x_0 x_1).iff_ (eq_ y_0 y_1)))))))
+  is_proof_alt ((and_ (eq_ x_0 y_0) (eq_ x_1 y_1)).imp_
+    ((eq_ x_0 x_1).iff_ (eq_ y_0 y_1)))
 
 -- ⊢ P ⇒ ⊢ ∀ v P
 | gen_
