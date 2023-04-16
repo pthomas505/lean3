ea0efb9c9b75ee24aa583cb94bc13b70c2f85376
Patrick Thomas <pthomas505@gmail.com>
2023-04-15 22:21:30 -0700

    Update.

--- src/metalogic/fol/no_sub_axiom/fol.lean
+++ src/metalogic/fol/no_sub_axiom/fol.lean
@@ -7,4 +7,24 @@ set_option pp.parens true
 open formula
 
 
+theorem eq_symm
+  (x y : variable_) :
+  is_proof ((eq_ x y).imp_ (eq_ y x)) :=
+begin
+  apply is_proof.mp_ (eq_ y y),
+  {
+    apply is_proof.mp_ ((eq_ y y).imp_ ((eq_ x y).imp_ ((eq_ y x).imp_ (eq_ y y)))),
+    {
+      sorry,
+    },
+    {
+      exact is_proof.eq_2_eq_ y x y y,
+    }
+  },
+  {
+    exact is_proof.eq_1_ y,
+  },
+end
+
+
 #lint
